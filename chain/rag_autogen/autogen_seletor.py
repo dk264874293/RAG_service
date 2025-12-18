7229d1005e607b3b5dfbccc003ace2a5fdfbc028
汪培良 <rick_wang@yunquna.com>
2025-12-18 13:30:05 +0800

    autogen_selector

--- chain/rag_autogen/autogen_seletor.py
+++ chain/rag_autogen/autogen_seletor.py
@@ -2,13 +2,14 @@
 Author: 汪培良 rick_wang@yunquna.com
 Date: 2025-12-18 11:41:48
 LastEditors: 汪培良 rick_wang@yunquna.com
-LastEditTime: 2025-12-18 12:37:04
+LastEditTime: 2025-12-18 13:27:44
 FilePath: /RAG_service/chain/rag_autogen/autogen_seletor.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 import asyncio
 import os
 from dotenv import load_dotenv
+from typing import Sequence
 from autogen_ext.models.openai import OpenAIChatCompletionClient
 from autogen_agentchat.agents import AssistantAgent, UserProxyAgent
 from autogen_agentchat.teams import RoundRobinGroupChat, SelectorGroupChat
@@ -16,6 +17,7 @@ from autogen_agentchat.conditions import TextMentionTermination, MaxMessageTermi
 from autogen_core import CancellationToken
 from autogen_agentchat.ui import Console
 from autogen_agentchat.messages import HandoffMessage
+from autogen_agentchat.messages import BaseAgentEvent, BaseChatMessage
 
 load_dotenv()
 
@@ -38,40 +40,79 @@ model_client = OpenAIChatCompletionClient(
 )
 
 async def main() -> None:
-    async def lookup_hotel(location:str) -> str:
-        return f"在{location}有三家酒店分别为'酒店A','酒店B','酒店C'"
+    def check_calculation(x:int,y:int,answer:int) -> str:
+        if x + y == answer:
+            return "Correct!"
+        else:
+            return "Incorrect!"
     
-    async def lookup_flight(origin:  str, destination: str) -> str:
-        return f"从{origin}到{destination}有'航班A','航班B','航班C'"
+    agent1 = AssistantAgent(
+        'Assistant_1', model_client=model_client,
+        description="For calculation",
+        system_message="Calculate the sum of two numbers",
+    )
 
-    async def book_trip() -> str:
-        return "你的旅行已经预定好了"
-    
-    travel_advisor = AssistantAgent(
-        "Travel_Advisor",
-        model_client,
-        tools=[book_trip],
-        description="帮助制定旅行计划。",
+    agent2 = AssistantAgent(
+        'Assistant_2', model_client=model_client,
+        tools=[check_calculation],
+        description="For checking the calculation",
+        system_message="Check the answer and respond with 'Correct!' or 'Incorrect!'",
     )
-    hotel_agent = AssistantAgent(
-        "Hotel_Agent",
-        model_client,
-        tools=[lookup_hotel],
-        description="帮助预约酒店",
-    )
-    flight_agent = AssistantAgent(
-        "Flight_Agent",
-        model_client,
-        tools=[lookup_flight],
-        description="帮助预约飞机航班",
-    )
-    termination = TextMentionTermination("TERMINATE")
+
+    def selector_func(messages:Sequence[BaseAgentEvent | BaseChatMessage]) -> str | None:
+        if len(messages) == 1 or messages[-1].to_text() == "Incorrect":
+            return "Assistant_1"
+        if messages[-1].source == "Assistant_1":
+            return "Assistant_2"
+        return None
+    termination = TextMentionTermination("Correct!")
 
     team = SelectorGroupChat(
-        [travel_advisor, hotel_agent, flight_agent],
+        [agent1, agent2],
         model_client=model_client,
+        selector_func=selector_func,
         termination_condition=termination,
     )
-    await Console(team.run_stream(task="制定一个到北京游玩3天的计划"))
+
+    await Console(team.run_stream(
+        task="计算1+1等于几"
+    ))
+    
+
+    # async def lookup_hotel(location:str) -> str:
+    #     return f"在{location}有三家酒店分别为'酒店A','酒店B','酒店C'"
+    
+    # async def lookup_flight(origin:  str, destination: str) -> str:
+    #     return f"从{origin}到{destination}有'航班A','航班B','航班C'"
+
+    # async def book_trip() -> str:
+    #     return "你的旅行已经预定好了"
+    
+    # travel_advisor = AssistantAgent(
+    #     "Travel_Advisor",
+    #     model_client,
+    #     tools=[book_trip],
+    #     description="帮助制定旅行计划。",
+    # )
+    # hotel_agent = AssistantAgent(
+    #     "Hotel_Agent",
+    #     model_client,
+    #     tools=[lookup_hotel],
+    #     description="帮助预约酒店",
+    # )
+    # flight_agent = AssistantAgent(
+    #     "Flight_Agent",
+    #     model_client,
+    #     tools=[lookup_flight],
+    #     description="帮助预约飞机航班",
+    # )
+    # termination = TextMentionTermination("TERMINATE")
+
+    # team = SelectorGroupChat(
+    #     [travel_advisor, hotel_agent, flight_agent],
+    #     model_client=model_client,
+    #     termination_condition=termination,
+    # )
+    # await Console(team.run_stream(task="制定一个到北京游玩3天的计划"))
 
 asyncio.run(main())
